bb0a2e102dfe5156c0c2e7317c50ee62af3a17f2
Eric <byungwook.ahn@gmail.com>
2018-02-05 17:36:07 +0900

    Modify cmd_cef_build.sh

--- cmd_cef_build.sh
+++ cmd_cef_build.sh
@@ -2,11 +2,23 @@
 
 for FILENAME in *.bz2; do
     #echo ${FILENAME%%.bz2*}
-    CEF_NAME=${FILENAME%%.bz2*}
+    CEF_NAME=${FILENAME%%.tar.bz2}
     echo "####################################"
     echo "Extracting the $CEF_NAME using bzip2"
     echo "####################################"
     break
 done
 
-bzip2 -cd $CEF_NAME.bz2 | tar xvf - 
+bzip2 -cd $CEF_NAME.tar.bz2 | tar xvf - 
+
+echo "####################################"
+echo " Build the CEF lib"
+echo "####################################"
+cd $CEF_NAME
+cmake .
+
+echo "####################################"
+echo " Build dll wrapper"
+echo "####################################"
+cd libcef_dll_wrapper
+make
